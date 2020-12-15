1fa9f565652e5f8fe92295ae7370a2751e9c2268
Anurag Kulkarni <anurag.kulkarni19@vit.edu>
2020-12-15 13:00:34 +0530

    Payment Gateway Integration Complete

--- routes/payment.js
+++ routes/payment.js
@@ -2,17 +2,20 @@ const express = require('express');
 const router = express.Router();
 const { PaymentGateway } = require('cashfree-sdk');
 
+let pg = new PaymentGateway({
+	env: 'TEST',
+	apiVersion: '1.0.0',
+	appId: process.env.CASHFREE_APPID,
+	secretKey: process.env.CASHFREE_SECRET_KEY,
+});
+
 router.post('/request', (req,res) => {
-	const pg = new PaymentGateway({
-		env: 'TEST',
-		apiVersion: '1.0.0',
-		appId: process.env.CASHFREE_APPID,
-		secretKey: process.env.CASHFREE_SECRET_KEY,
-	});
+
+	const orderId = '230';
 
 	pg.orders
 	.createOrders({
-		orderId: '115',
+		orderId: orderId,
 		orderAmount: '100',
 		orderCurrency: 'INR',
 		orderNote: 'Subscription',
@@ -20,16 +23,30 @@ router.post('/request', (req,res) => {
 		customerPhone: '9111122222',
 		customerEmail: 'johndoe@cashfree.com',
 		sellerPhone: '9900887766',
-		returnUrl: 'http://localhost:5000/response',
+		returnUrl: 'http://localhost:5000/payments/response/'+orderId,
 		notifyUrl: 'http://localhost:5000/notify',
 		paymentModes: '',
 		pc: '',
 	})
 	.then((data) => {
+		if(data.status == "ERROR") return res.status(400).render('error',{msg: data.reason});
 		const url = data.paymentLink;
 		res.render('request', {url: url})
 	})
 	.catch((error) => console.error(error));
 });
+
+router.post('/response/:id', (req,res) => {
+
+	const orderid = req.params.id;
+	pg.orders
+	.getStatus({
+		orderId: orderid,
+	})
+	.then((data) => {
+		res.render('response', {status: data.txStatus, message: data.txMsg});
+	})
+	.catch((error) => console.error(error));
+})
 
 module.exports = router;
